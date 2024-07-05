e163fbec2783538410f2d2c2fab283a2bf9b9e12
tnosh73 <tnosh73@gmail.com>
2024-07-05 10:19:19 +0900

    fix: rename

--- src/main/java/com/newdeal/staynest/service/MemberServiceImpl.java
+++ src/main/java/com/newdeal/staynest/service/MemberServiceImpl.java
@@ -1,10 +1,9 @@
 package com.newdeal.staynest.service;
 
-import com.newdeal.staynest.dto.MemberDto;
-import com.newdeal.staynest.entity.Member;
+import com.newdeal.staynest.dto.GuestDto;
+import com.newdeal.staynest.entity.Guest;
 import com.newdeal.staynest.entity.UserRoleEnum;
 import com.newdeal.staynest.repository.MemberRepository;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -26,23 +25,23 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public String checkDuplicateEmail(String email) {
-        Optional<Member> member = memberRepository.findByEmail(email);
+        Optional<Guest> member = Optional.ofNullable(memberRepository.findByEmail(email));
 
         return member.isPresent() ? "이미 등록된 이메일입니다." : "사용가능한 이메일입니다.";
     }
 
     @Override
-    public void registerMember(MemberDto memberDto) {
+    public void registerMember(GuestDto guestDto) {
         // 비밀번호 암호화
-        String encodedPassword = passwordEncoder.encode(memberDto.getPassword());
-        memberDto.setPassword(encodedPassword);
+        String encodedPassword = passwordEncoder.encode(guestDto.getPassword());
+        guestDto.setPassword(encodedPassword);
         // 기본 롤 세팅-GUEST임
-        memberDto.setRole(UserRoleEnum.GUEST);
+        guestDto.setRole(UserRoleEnum.GUEST);
 
         // Member 엔티티로 변환
-        Member member = MemberDto.toEntity(memberDto);
+        Guest guest = GuestDto.toEntity(guestDto);
 
-        memberRepository.save(member);
+        memberRepository.save(guest);
     }
 
 //    @Override
